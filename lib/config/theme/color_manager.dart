350f66b70396fad1d1b75ff8c545253b311fecad
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-09-27 02:08:19 -0500

    agregar paletas de colores

--- lib/config/theme/color_manager.dart
+++ lib/config/theme/color_manager.dart
@@ -31,3 +31,37 @@ class ColorPalette1 {
   static const Color color7 = Color(0xFF00e4ff);
   static const Color color7a = Color(0xFF59D9DB);
 }
+
+class ColorPalette2 {
+  static const Color color1 = Color(0xff8184ce);
+  static const Color color1a = Color(0xff8080ff);
+  static const Color color2 = Color(0xff66d0d2);
+  static const Color color2a = Color(0xffC280ff);
+  static const Color color3 = Color(0xff00d581);
+  static const Color color3a = Color(0xfff59a23);
+  static const Color color4 = Color(0xff49d854);
+  static const Color color4a = Color(0xff027db4);
+  static const Color color5 = Color(0xffffc32d);
+  static const Color color5a = Color(0xff70b603);
+  static const Color color6 = Color(0xFFf7803e);
+  static const Color color6a = Color(0xFFb67903);
+  static const Color color7 = Color(0xFFf04f4f);
+  static const Color color7a = Color(0xFF6866cc);
+}
+
+class ColorPalette3 {
+  static const Color color1 = Color(0xff5e61be);
+  static const Color color1a = Color(0xff080ff8);
+  static const Color color2 = Color(0xff4f7c90);
+  static const Color color2a = Color(0xffC280ff);
+  static const Color color3 = Color(0xff3d9b5b);
+  static const Color color3a = Color(0xfff59a23);
+  static const Color color4 = Color(0xff4fab38);
+  static const Color color4a = Color(0xff027db4);
+  static const Color color5 = Color(0xff94a52f);
+  static const Color color5a = Color(0xff70b603);
+  static const Color color6 = Color(0xFFd19728);
+  static const Color color6a = Color(0xFFb67903);
+  static const Color color7 = Color(0xFFd8612a);
+  static const Color color7a = Color(0xFF6866cc);
+}
