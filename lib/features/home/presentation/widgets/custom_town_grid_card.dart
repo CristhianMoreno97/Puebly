ddc87839796db7ff7c251c1b8047d00b91c9c547
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-11-09 15:13:14 -0500

    enlazar targetas de pueblos con enlaces web

--- lib/features/home/presentation/widgets/custom_town_grid_card.dart
+++ lib/features/home/presentation/widgets/custom_town_grid_card.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:puebly/config/constants/enviroment_constants.dart';
 import 'package:puebly/config/theme/color_manager.dart';
+import 'package:puebly/features/home/presentation/providers/page_controller_provider.dart';
+import 'package:puebly/features/home/presentation/providers/utils_provider.dart';
+import 'package:puebly/features/home/presentation/providers/webview_providers.dart';
 
-class CustomTownGridCard extends StatelessWidget {
-  const CustomTownGridCard({super.key,
+class CustomTownGridCard extends ConsumerWidget {
+  const CustomTownGridCard({
+    super.key,
     required this.title,
     required this.description,
     required this.imagePath,
     required this.townId,
-    required this.enabled
+    required this.enabled,
   });
 
   final String title;
@@ -18,60 +23,79 @@ class CustomTownGridCard extends StatelessWidget {
   final bool enabled;
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.only(
+  Widget build(BuildContext context, WidgetRef ref) {
+    void goToSection(int sectionIndex) {
+      ref.read(showHomeScreenProvider.notifier).state = false;
+      ref.read(pageControllerProvider.notifier).update((state) {
+        state.animateToPage(sectionIndex,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut);
+        return state;
+      });
+    }
+
+    return GestureDetector(
+      onTap: () {
+        ref.read(townParameter.notifier).state = townId;
+        goToSection(0);
+      },
+      child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(0),
               topRight: Radius.circular(16),
               bottomLeft: Radius.circular(16),
               bottomRight: Radius.circular(0),
             ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 4,
-            blurRadius: 8,
-            offset: const Offset(0, 0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 4,
+                blurRadius: 8,
+                offset: const Offset(0, 0),
+              ),
+            ],
           ),
-        ],
-      ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        TownImage(imagePath: imagePath),
-        const SizedBox(height: 8),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Text(
-            title,
-            style: TextStyle(
-              color: enabled ? ColorManager.pueblySecundary1 : ColorManager.pueblySecundary1,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        const SizedBox(height: 4),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Text(
-            description,
-            style: TextStyle(
-              letterSpacing: 0,
-              wordSpacing: 0,
-              height: 1.2,
-              color: enabled ? ColorManager.pueblyPrimary2b : ColorManager.pueblyPrimary2c,
-              fontSize: 12,
-            ),
-          ),
-        ),
-        const SizedBox(height: 16),
-      ],
-    )
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TownImage(imagePath: imagePath),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: enabled
+                        ? ColorManager.pueblySecundary1
+                        : ColorManager.pueblySecundary1,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 4),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  description,
+                  style: TextStyle(
+                    letterSpacing: 0,
+                    wordSpacing: 0,
+                    height: 1.2,
+                    color: enabled
+                        ? ColorManager.pueblyPrimary2b
+                        : ColorManager.pueblyPrimary2c,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+            ],
+          )),
     );
   }
 }
@@ -90,17 +114,17 @@ class TownImage extends StatelessWidget {
           width: width,
           height: width,
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage("${EnviromentConstants.homeURL}/$imagePath"),
-              fit: BoxFit.cover,
-            ),
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(0),
-              topRight: Radius.circular(16),
-              bottomLeft: Radius.circular(16),
-              bottomRight: Radius.circular(0),
-            )
-          ),
+              image: DecorationImage(
+                image:
+                    NetworkImage("${EnviromentConstants.homeURL}/$imagePath"),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(0),
+                topRight: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(0),
+              )),
         );
       },
     );
