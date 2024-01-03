8515d4c4355e4b791cbac827d922a1d240a3f344
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-03 18:41:06 -0500

    Remove unused imports and code in HomeScreen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:go_router/go_router.dart';
 import 'package:puebly/config/theme/color_manager.dart';
-import 'package:puebly/features/home/presentation/providers/auxiliary_webview_providers.dart';
-import 'package:puebly/features/home/presentation/providers/page_controller_provider.dart';
 import 'package:puebly/features/home/presentation/providers/towns_provider.dart';
-import 'package:puebly/features/home/presentation/providers/utils_provider.dart';
 import 'package:puebly/features/home/presentation/widgets/appbar_title.dart';
 import 'package:puebly/features/home/presentation/widgets/custom_home_card.dart';
 import 'package:puebly/features/home/presentation/widgets/custom_town_grid_card.dart';
@@ -15,98 +11,7 @@ class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    void goToSection(int sectionIndex) {
-      ref.read(showHomeScreenProvider.notifier).state = false;
-      ref.read(pageControllerProvider.notifier).update((state) {
-        state.animateToPage(sectionIndex,
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.easeInOut);
-        return state;
-      });
-    }
-
-    Widget buildSectionButton(
-        String label, IconData icon, int? index, String? webViewPath) {
-      return IconButton(
-        onPressed: () {},
-        icon: Icon(
-          icon,
-          size: 32,
-          color: Colors.white,
-        ),
-      );
-    }
-
-    Widget buildCustomCard(
-        Color color,
-        String label,
-        IconData? icon,
-        String description,
-        int? index,
-        String? webViewPath,
-        String? imagePath) {
-      return GestureDetector(
-        onTap: () {
-          if (index != null) {
-            goToSection(index);
-            return;
-          }
-          if (webViewPath != null) {
-            ref.read(auxiliaryWebViewPathProvider.notifier).state = webViewPath;
-            context.push('/auxiliary-screen');
-          }
-        },
-        child: Card(
-          color: color,
-          elevation: 0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // image avatar
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(500),
-                    child: Image.asset(
-                      width: 48,
-                      height: 48,
-                      imagePath ?? 'assets/images/placeholder_1.jpg',
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 220,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          label,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          description,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  buildSectionButton(
-                      "-", icon ?? Icons.abc, index, webViewPath),
-                ]),
-          ),
-        ),
-      );
-    }
-
+  Widget build(BuildContext context, WidgetRef ref) { 
     return Scaffold(
       appBar: AppBar(
         surfaceTintColor: Colors.transparent,
