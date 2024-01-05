faab611aa806068332e6cd5c7ac12cf0500d7c6a
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-05 18:49:31 -0500

    Add CustomAppBar to TownsScreen

--- lib/features/towns/presentation/screens/towns_screen.dart
+++ lib/features/towns/presentation/screens/towns_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:puebly/config/theme/color_manager.dart';
 import 'package:puebly/features/towns/presentation/providers/towns_provider.dart';
+import 'package:puebly/features/towns/presentation/widgets/custom_appbar.dart';
 import 'package:puebly/features/towns/presentation/widgets/town_card.dart';
 import 'package:puebly/features/towns/presentation/widgets/welcome_section.dart';
 
@@ -12,39 +13,13 @@ class TownsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: _CustomAppBar(),
+      appBar: CustomAppBar(),
       body: _MainView(),
     );
   }
 }
 
-class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const _CustomAppBar();
 
-  @override
-  Size get preferredSize => const Size.fromHeight(56);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: const _AppBarTitle(),
-      elevation: 0,
-      centerTitle: true,
-      backgroundColor: ColorManager.pueblyPrimary1,
-    );
-  }
-}
-
-class _AppBarTitle extends StatelessWidget {
-  const _AppBarTitle();
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipRect(
-      child: Image.asset('assets/images/logo-puebly.png', height: 40),
-    );
-  }
-}
 
 class _MainView extends StatelessWidget {
   const _MainView();
