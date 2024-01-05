ffe6cf351f088cee49e1646e4dce7bccdc1bc7e6
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-05 18:49:24 -0500

    Add CustomAppBar widget to the towns feature

---
+++ lib/features/towns/presentation/widgets/custom_appbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:puebly/config/theme/color_manager.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const _AppBarTitle(),
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: ColorManager.pueblyPrimary1,
+    );
+  }
+}
+
+class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: Image.asset('assets/images/logo-puebly.png', height: 40),
+    );
+  }
+}
