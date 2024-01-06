d776dac032a9036eb2aa54722462887804b4af3b
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-05 19:23:22 -0500

    Add drawer

--- lib/features/towns/presentation/screens/town_sections_screen.dart
+++ lib/features/towns/presentation/screens/town_sections_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:puebly/features/home/presentation/widgets/custom_drawer.dart';
+import 'package:puebly/features/towns/presentation/widgets/custom_appbar.dart';
 
-class TownSectionsScreen extends StatelessWidget {
+class TownSectionsScreen extends ConsumerWidget {
   final String townId;
   const TownSectionsScreen({super.key, required this.townId});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text(townId),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      key: GlobalKey<ScaffoldState>(),
+      drawer: const CustomDrawer(),
+      appBar: const CustomAppBar(),
+      body: Text(townId),
     );
   }
 }
