3228491c9ff74cd74773934dce8892b1424535bb
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-05 19:21:08 -0500

    Add custom drawer to TownsScreen

--- lib/features/towns/presentation/screens/towns_screen.dart
+++ lib/features/towns/presentation/screens/towns_screen.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:puebly/config/theme/color_manager.dart';
+import 'package:puebly/features/home/presentation/widgets/custom_drawer.dart';
 import 'package:puebly/features/towns/presentation/providers/towns_provider.dart';
 import 'package:puebly/features/towns/presentation/widgets/custom_appbar.dart';
 import 'package:puebly/features/towns/presentation/widgets/town_card.dart';
 import 'package:puebly/features/towns/presentation/widgets/welcome_section.dart';
 
-class TownsScreen extends StatelessWidget {
+class TownsScreen extends ConsumerWidget {
   const TownsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
-      body: _MainView(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      key: GlobalKey<ScaffoldState>(),
+      drawer: const CustomDrawer(),
+      appBar: const CustomAppBar(),
+      body: const _MainView(),
     );
   }
 }
