04d18f189d1aa55b47ecd3a2fb260113c567616d
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-06 11:13:11 -0500

    Add sections providers and widgets to TownSectionsScreen

--- lib/features/towns/presentation/screens/town_sections_screen.dart
+++ lib/features/towns/presentation/screens/town_sections_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:puebly/features/home/presentation/widgets/custom_drawer.dart';
+import 'package:puebly/features/towns/presentation/providers/sections_providers.dart';
 import 'package:puebly/features/towns/presentation/widgets/custom_appbar.dart';
+import 'package:puebly/features/towns/presentation/widgets/sections_bottom_navbar.dart';
+import 'package:puebly/features/towns/presentation/widgets/town_sections_info.dart';
 
 class TownSectionsScreen extends StatelessWidget {
   final String townId;
@@ -12,7 +16,29 @@ class TownSectionsScreen extends StatelessWidget {
       key: GlobalKey<ScaffoldState>(),
       drawer: const CustomDrawer(),
       appBar: const CustomAppBar(),
-      body: Text(townId),
+      body: _MainView(townId: townId),
+      bottomNavigationBar: const SectionsBottomNavBar(),
+    );
+  }
+}
+
+class _MainView extends ConsumerWidget {
+  final String townId;
+
+  const _MainView({required this.townId});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return PageView.builder(
+      controller: ref.watch(sectionsPageControllerProvider),
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: TownSectionsInfo.sections.length,
+      itemBuilder: (context, index) {
+        return Text(TownSectionsInfo.sections[index].name);
+      },      
+      onPageChanged: (index) {
+        ref.read(selectedSectionIndexProvider.notifier).state =index;
+      },
     );
   }
 }
