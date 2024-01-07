522b8640cb802d4f17db9df7747c2fc39695603f
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-06 19:40:32 -0500

    Add getNewerPosts method to TownsDataSource and TownsRepository

--- lib/features/towns/domain/datasources/towns_datasource.dart
+++ lib/features/towns/domain/datasources/towns_datasource.dart
@@ -1,5 +1,7 @@
+import 'package:puebly/features/towns/infraestructure/models/post_model.dart';
 import 'package:puebly/features/towns/infraestructure/models/town_model.dart';
 
 abstract class TownsDataSource {
   Future<List<TownModel>> getTowns(int page);
+  Future<List<PostModel>> getNewerPosts(int townCategoryId,int page);
 }
