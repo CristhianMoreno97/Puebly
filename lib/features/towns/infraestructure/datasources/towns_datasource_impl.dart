d9122f339fbbe0cb9f677eb97bfae3aa81f71f02
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-06 19:42:44 -0500

    Add PostModelMapper and implement getNewerPosts in TownsDataSourceImpl

--- lib/features/towns/infraestructure/datasources/towns_datasource_impl.dart
+++ lib/features/towns/infraestructure/datasources/towns_datasource_impl.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:puebly/config/constants/enviroment_constants.dart';
 import 'package:puebly/features/towns/domain/datasources/towns_datasource.dart';
+import 'package:puebly/features/towns/infraestructure/mappers/post_model_mapper.dart';
 import 'package:puebly/features/towns/infraestructure/mappers/town_model_mapper.dart';
+import 'package:puebly/features/towns/infraestructure/models/post_model.dart';
 import 'package:puebly/features/towns/infraestructure/models/town_model.dart';
 
 class TownsDataSourceImpl extends TownsDataSource {
@@ -20,6 +22,19 @@ class TownsDataSourceImpl extends TownsDataSource {
           .map((town) => TownModelMapper.fromJson(town))
           .toList();
       return towns;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<PostModel>> getNewerPosts(int townCategoryId, int page) async {
+    try {
+      final response = await _dio.get('/wp-json/api/v1/newer-post?t=$townCategoryId&p=$page');
+      final List<PostModel> posts = (response.data as List)
+          .map((post) => PostModelMapper.fromJson(post))
+          .toList();
+      return posts;
     } catch (e) {
       rethrow;
     }
--- lib/features/towns/infraestructure/repositories/towns_repository_impl.dart
+++ lib/features/towns/infraestructure/repositories/towns_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:puebly/features/towns/domain/datasources/towns_datasource.dart';
+import 'package:puebly/features/towns/domain/entities/post.dart';
 import 'package:puebly/features/towns/domain/entities/town.dart';
 import 'package:puebly/features/towns/domain/repositories/towns_repository.dart';
 
@@ -19,5 +20,27 @@ class TownsRepositoryImpl extends TownsRepository {
             enabled: town.enabled,
             categoryId: town.categoryId))
         .toList();
+  }
+
+  @override
+  Future<Map<int, List<Post>>> getNewerPosts(int townCategoryId, int page) async {
+    try {
+      final postModels = await _townsDataSource.getNewerPosts(townCategoryId, page);
+      final newerPostByCategory = <int, List<Post>>{};
+      for (final postModel in postModels) {
+        final categoryId = postModel.sectionCategoryId;
+        newerPostByCategory[categoryId] ??= [];
+        newerPostByCategory[categoryId]!.add(Post(
+            id: postModel.id,
+            title: postModel.title,
+            content: postModel.content,
+            featuredImgUrl: postModel.featuredImgUrl,
+            images: postModel.images,
+            categories: postModel.categories));
+      }
+      return newerPostByCategory;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
