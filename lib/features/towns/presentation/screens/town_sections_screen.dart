c0988660e561c94c927b143c1fcff0fce94182e9
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-06 19:43:31 -0500

    Refactor townCategoryId to townId in TownSectionsScreen

--- lib/features/towns/presentation/screens/town_sections_screen.dart
+++ lib/features/towns/presentation/screens/town_sections_screen.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:puebly/features/home/presentation/widgets/custom_drawer.dart';
 import 'package:puebly/features/towns/presentation/providers/sections_providers.dart';
+import 'package:puebly/features/towns/presentation/providers/town_provider.dart';
 import 'package:puebly/features/towns/presentation/widgets/custom_appbar.dart';
 import 'package:puebly/features/towns/presentation/widgets/sections_bottom_navbar.dart';
 import 'package:puebly/features/towns/presentation/widgets/town_sections_info.dart';
 
 class TownSectionsScreen extends StatelessWidget {
-  final String townCategoryId;
-  const TownSectionsScreen({super.key, required this.townCategoryId});
+  final String townId;
+  const TownSectionsScreen({super.key, required this.townId});
 
   @override
   Widget build(BuildContext context) {
+    final townCategoryId = int.parse(townId);
     return Scaffold(
       key: GlobalKey<ScaffoldState>(),
       drawer: const CustomDrawer(),
@@ -23,7 +25,7 @@ class TownSectionsScreen extends StatelessWidget {
 }
 
 class _MainView extends ConsumerWidget {
-  final String townCategoryId;
+  final int townCategoryId;
 
   const _MainView({required this.townCategoryId});
 
@@ -34,7 +36,7 @@ class _MainView extends ConsumerWidget {
       physics: const NeverScrollableScrollPhysics(),
       itemCount: TownSectionsInfo.sections.length,
       itemBuilder: (context, index) {
-        return _SectionContent(index);
+        return _SectionContent(townCategoryId, index);
       },
       onPageChanged: (index) {
         ref.read(selectedSectionIndexProvider.notifier).state = index;
@@ -43,12 +45,22 @@ class _MainView extends ConsumerWidget {
   }
 }
 
-class _SectionContent extends StatelessWidget {
+class _SectionContent extends ConsumerWidget {
+  final int townCategoryId;
   final int index;
-  const _SectionContent(this.index);
+  const _SectionContent(this.townCategoryId, this.index);
 
   @override
-  Widget build(BuildContext context) {
-    return Text(TownSectionsInfo.sections[index].name);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final posts = ref.watch(townProvider(townCategoryId));
+    final sectionPosts = posts.townSections[index].posts;
+    return ListView.builder(
+      itemCount: sectionPosts.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(sectionPosts[index].title),
+        );
+      },
+    );
   }
 }
