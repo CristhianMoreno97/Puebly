ab375905296d9a04130a72d7b4e267776fd5da3b
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-06 19:43:18 -0500

    Refactor TownSection class to use SectionInfo object

--- lib/features/towns/domain/entities/town_section.dart
+++ lib/features/towns/domain/entities/town_section.dart
@@ -1,41 +1,41 @@
-import 'package:flutter/material.dart';
 import 'package:puebly/features/towns/domain/entities/post.dart';
+import 'package:puebly/features/towns/presentation/widgets/town_sections_info.dart';
 
 class TownSection {
-  final int categoryId;
-  final String name;
-  final String description;
-  final String featuredImgUrl;
-  final IconData icon;
-  final Color color;
+  final SectionInfo info;
+  //final String name;
+  //final String description;
+  //final String featuredImgUrl;
+  //final IconData icon;
+  //final Color color;
   final List<Post> posts;
 
   TownSection({
-    required this.categoryId,
-    required this.name,
-    required this.description,
-    required this.featuredImgUrl,
-    required this.icon,
-    required this.color,
+    required this.info,
+    //required this.name,
+    //required this.description,
+    //required this.featuredImgUrl,
+    //required this.icon,
+    //required this.color,
     this.posts = const [],
   });
 
   TownSection copyWith({
-    int? categoryId,
-    String? name,
-    String? description,
-    String? featuredImgUrl,
-    IconData? icon,
-    Color? color,
+    SectionInfo? info,
+    //String? name,
+    //String? description,
+    //String? featuredImgUrl,
+    //IconData? icon,
+    //Color? color,
     List<Post>? posts,
   }) {
     return TownSection(
-      categoryId: categoryId ?? this.categoryId,
-      name: name ?? this.name,
-      description: description ?? this.description,
-      featuredImgUrl: featuredImgUrl ?? this.featuredImgUrl,
-      icon: icon ?? this.icon,
-      color: color ?? this.color,
+      info: info ?? this.info,
+      //name: name ?? this.name,
+      //description: description ?? this.description,
+      //featuredImgUrl: featuredImgUrl ?? this.featuredImgUrl,
+      //icon: icon ?? this.icon,
+      //color: color ?? this.color,
       posts: posts ?? this.posts,
     );
   }
