5e05bc885e0a540ffde722aaade31f0c5f68eead
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-06 21:55:41 -0500

    Add MasonryGridView and PostCard to SectionContent

--- lib/features/towns/presentation/screens/town_sections_screen.dart
+++ lib/features/towns/presentation/screens/town_sections_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:puebly/features/home/presentation/widgets/custom_drawer.dart';
 import 'package:puebly/features/towns/presentation/providers/sections_providers.dart';
 import 'package:puebly/features/towns/presentation/providers/town_provider.dart';
 import 'package:puebly/features/towns/presentation/widgets/custom_appbar.dart';
+import 'package:puebly/features/towns/presentation/widgets/post_card.dart';
 import 'package:puebly/features/towns/presentation/widgets/sections_bottom_navbar.dart';
 import 'package:puebly/features/towns/presentation/widgets/town_sections_info.dart';
 
@@ -47,20 +49,20 @@ class _MainView extends ConsumerWidget {
 
 class _SectionContent extends ConsumerWidget {
   final int townCategoryId;
-  final int index;
-  const _SectionContent(this.townCategoryId, this.index);
+  final int pageIndex;
+  const _SectionContent(this.townCategoryId, this.pageIndex);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final posts = ref.watch(townProvider(townCategoryId));
-    final sectionPosts = posts.townSections[index].posts;
-    return ListView.builder(
-      itemCount: sectionPosts.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(sectionPosts[index].title),
-        );
-      },
-    );
+    final sectionPosts = posts.townSections[pageIndex].posts;
+    return MasonryGridView.count(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        mainAxisSpacing: 16,
+        crossAxisCount: 1,
+        itemCount: sectionPosts.length,
+        itemBuilder: (context, index) {
+          return PostCard(post: sectionPosts[index]);
+        });
   }
 }
