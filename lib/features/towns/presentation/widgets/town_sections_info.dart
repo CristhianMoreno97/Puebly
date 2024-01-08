4b5293761b9d222c45d9d3d9cf895e6feac3c1f9
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-08 11:50:19 -0500

    Add featured image, color, and description to SectionInfo

--- lib/features/towns/presentation/widgets/town_sections_info.dart
+++ lib/features/towns/presentation/widgets/town_sections_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:puebly/config/theme/color_manager.dart';
 
 class TownSectionsInfo {
   static List<SectionInfo> get sections => const [
@@ -7,42 +8,63 @@ class TownSectionsInfo {
           selectedIcon: Icons.storefront_rounded,
           unselectedIcon: Icons.storefront_outlined,
           categoryId: 3,
+          featuredImage: 'assets/images/Comercio.jpg',
+          color: ColorPalette4.color7,
+          description: "Conoce los mejores restaurantes, tiendas, hoteles y negocios locales.",
         ),
         SectionInfo(
           name: 'Turismo',
           selectedIcon: Icons.place_rounded,
           unselectedIcon: Icons.place_outlined,
           categoryId: 4,
+          featuredImage: 'assets/images/Turismo.jpg',
+          color: ColorPalette4.color6,
+          description: "Explora los puntos turísticos que ofrece nuestro hermoso municipio.",
         ),
         SectionInfo(
           name: 'Plaza',
           selectedIcon: Icons.shopping_basket_rounded,
           unselectedIcon: Icons.shopping_basket_outlined,
           categoryId: 7,
+          featuredImage: 'assets/images/Plaza.jpg',
+          color: ColorPalette4.color5,
+          description: "Consigue una amplia variedad de hortalizas, tubérculos, hierbas y animales, al por mayor y al detalle.",
         ),
         SectionInfo(
           name: 'Empleo',
           selectedIcon: Icons.work_outlined,
           unselectedIcon: Icons.work_outline_outlined,
           categoryId: 5,
+          featuredImage: 'assets/images/empleo.jpg',
+          color: ColorPalette4.color4,
+          description: "Encuentra ofertas de trabajo por jornal en diversas tareas del campo.",
         ),
         SectionInfo(
           name: 'Anuncios',
           selectedIcon: Icons.sell_rounded,
           unselectedIcon: Icons.sell_outlined,
           categoryId: 35,
+          featuredImage: 'assets/images/Clasificados.jpg',
+          color: ColorPalette4.color3,
+          description: "Infórmate sobre servicios de finca raíz, acarreos, construcción, aseo, maquinaria y mucho más.",
         ),
         SectionInfo(
           name: 'Comunidad',
           selectedIcon: Icons.group_rounded,
           unselectedIcon: Icons.group_outlined,
           categoryId: 36,
+          featuredImage: 'assets/images/Comunidad.jpg',
+          color: ColorPalette4.color2,
+          description: "Entérate de anuncios de la alcaldía, fechas del pago adulto mayor, entre otros.",
         ),
         SectionInfo(
           name: 'Sabías que',
           selectedIcon: Icons.menu_book_rounded,
           unselectedIcon: Icons.menu_book_rounded,
           categoryId: 37,
+          featuredImage: 'assets/images/Sabías que.jpg',
+          color: ColorPalette4.color1,
+          description: "Descubre datos interesantes y curiosos sobre nuestro municipio y su historia.",
         ),
       ];
 }
@@ -52,11 +74,17 @@ class SectionInfo {
   final IconData selectedIcon;
   final IconData unselectedIcon;
   final int categoryId;
+  final String featuredImage;
+  final Color color;
+  final String description;
 
   const SectionInfo({
     required this.name,
     required this.selectedIcon,
     required this.unselectedIcon,
     required this.categoryId,
+    required this.featuredImage,
+    required this.color,
+    required this.description,
   });
 }
