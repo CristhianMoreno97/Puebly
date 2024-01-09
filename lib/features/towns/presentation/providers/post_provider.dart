8f0a2e0ad0430ab951ae1f3439d85737c88cfcde
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-09 10:17:37 -0500

    Add post provider for towns feature

---
+++ lib/features/towns/presentation/providers/post_provider.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:puebly/features/towns/domain/entities/post.dart';
+
+final postProvider = StateProvider<Post?>((ref) => null);
