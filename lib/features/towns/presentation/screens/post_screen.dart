b84679c356da8a717af818796b653ab7c92727ef
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-09 10:25:19 -0500

    Add post screen with content view and image viewer
    se creo la funcion dividirCadena para determinar en que momento colocar las imagenes del post

--- lib/features/towns/presentation/screens/post_screen.dart
+++ lib/features/towns/presentation/screens/post_screen.dart
@@ -1,18 +1,112 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:puebly/features/towns/domain/entities/post.dart';
+import 'package:puebly/features/towns/presentation/providers/post_provider.dart';
 import 'package:puebly/features/towns/presentation/widgets/custom_appbar.dart';
 import 'package:puebly/features/towns/presentation/widgets/custom_drawer.dart';
 
-class PostScreen extends StatelessWidget {
+class PostScreen extends ConsumerWidget {
   final String postId;
   const PostScreen({super.key, required this.postId});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final post = ref.watch(postProvider);
+
     return Scaffold(
       appBar: const CustomAppBar(),
       drawer: const CustomDrawer(),
-      body: Center(
-        child: Text('Post Screen - $postId'),
+      body: post != null
+          ? _ContentView(post)
+          : const Text('\n¡Post no encontrado!'),
+    );
+  }
+}
+
+class _ContentView extends StatelessWidget {
+  final Post post;
+  const _ContentView(this.post);
+
+  List<String> dividirCadena(String cadena) {
+    List<String> lineas = cadena.trim().split('\n\n\n\n');
+
+    for (var i = 0; i < lineas.length; i++) {
+      lineas[i] = lineas[i].trim();
+
+      if (!(i+1 < lineas.length)) {
+        break;
+      }
+
+      if (lineas[i].isNotEmpty) {
+        if (lineas[i+1].isNotEmpty) {
+          lineas[i] += '\n${lineas[i+1]}';
+          lineas.removeAt(i+1);
+          i--;
+        }
+        continue;
+      }
+      
+      if (lineas[i + 1].isNotEmpty) {
+        lineas[i - 1] += '';
+        lineas.removeAt(i);
+        i--;
+      }
+    }
+    return lineas;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> postContent = dividirCadena(post.content);
+    final maxLength = postContent.length > post.images.length
+        ? postContent.length
+        : post.images.length;
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(post.title),
+          _ImageViewer(post.featuredImgUrl),
+          for (var i = 0; i < maxLength; i++)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (i < postContent.length && postContent[i].trim().isNotEmpty)
+                  Text(
+                    postContent[i],
+                    textAlign: TextAlign.left,
+                  ),
+                if (i < post.images.length) _ImageViewer(post.images[i]),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ImageViewer extends StatelessWidget {
+  final String imagePath;
+  const _ImageViewer(this.imagePath);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(16)),
+      child: CachedNetworkImage(
+        imageUrl: imagePath,
+        width: double.infinity,
+        fit: BoxFit.cover,
+        placeholder: (context, url) => Center(
+          child: ClipRRect(
+            child: Image.asset('assets/images/ph_2.jpg',
+                fit: BoxFit.cover, width: double.infinity),
+          ),
+        ),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
     );
   }
