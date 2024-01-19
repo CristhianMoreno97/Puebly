c8040376a5cb3d2f2238b3566dd17ae0fe67e301
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-18 23:01:25 -0500

    Refactor(town): Remove commented out code in TownSection class

--- lib/features/towns/domain/entities/town_section.dart
+++ lib/features/towns/domain/entities/town_section.dart
@@ -3,39 +3,19 @@ import 'package:puebly/features/towns/presentation/widgets/town_sections_info.da
 
 class TownSection {
   final SectionInfo info;
-  //final String name;
-  //final String description;
-  //final String featuredImgUrl;
-  //final IconData icon;
-  //final Color color;
   final List<Post> posts;
 
   TownSection({
     required this.info,
-    //required this.name,
-    //required this.description,
-    //required this.featuredImgUrl,
-    //required this.icon,
-    //required this.color,
     this.posts = const [],
   });
 
   TownSection copyWith({
     SectionInfo? info,
-    //String? name,
-    //String? description,
-    //String? featuredImgUrl,
-    //IconData? icon,
-    //Color? color,
     List<Post>? posts,
   }) {
     return TownSection(
       info: info ?? this.info,
-      //name: name ?? this.name,
-      //description: description ?? this.description,
-      //featuredImgUrl: featuredImgUrl ?? this.featuredImgUrl,
-      //icon: icon ?? this.icon,
-      //color: color ?? this.color,
       posts: posts ?? this.posts,
     );
   }
