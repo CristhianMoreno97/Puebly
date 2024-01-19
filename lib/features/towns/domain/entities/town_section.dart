c58244f159c8fb4bb80979325b096d6bfd7c1ea7
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-18 23:06:53 -0500

    feat(towns): add categories to TownSection entity
    Add 'categories' property to the TownSection entity to represent associated categories. Also, update the 'copyWith' method to handle the new property.

--- lib/features/towns/domain/entities/town_section.dart
+++ lib/features/towns/domain/entities/town_section.dart
@@ -1,22 +1,27 @@
+import 'package:puebly/features/towns/domain/entities/category.dart';
 import 'package:puebly/features/towns/domain/entities/post.dart';
 import 'package:puebly/features/towns/presentation/widgets/town_sections_info.dart';
 
 class TownSection {
   final SectionInfo info;
   final List<Post> posts;
+  final List<Category> categories;
 
   TownSection({
     required this.info,
     this.posts = const [],
+    this.categories = const [],
   });
 
   TownSection copyWith({
     SectionInfo? info,
     List<Post>? posts,
+    List<Category>? categories,
   }) {
     return TownSection(
       info: info ?? this.info,
       posts: posts ?? this.posts,
+      categories: categories ?? this.categories,
     );
   }
 }
