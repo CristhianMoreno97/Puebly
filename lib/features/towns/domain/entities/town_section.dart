b4de2c9e71482944c21c99309b028d954fa80b4f
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-20 23:29:33 -0500

    feat(town-section): expand TownSection with additional attributes
    Enhanced the `TownSection` class in the `widgets` scope to include the following attributes:
    - `isLoading` for indicating whether data is currently being loaded.
    - `isLastPage` for indicating if it's the last available page.
    - `page` to keep track of the current page.
    
    These additions provide better support for handling loading states and paginated data in the context of TownSections.

--- lib/features/towns/domain/entities/town_section.dart
+++ lib/features/towns/domain/entities/town_section.dart
@@ -6,22 +6,34 @@ class TownSection {
   final SectionInfo info;
   final List<Post> posts;
   final List<Category> childCategories;
+  final bool isLoading;
+  final bool isLastPage;
+  final int page;
 
   TownSection({
     required this.info,
     this.posts = const [],
     this.childCategories = const [],
+    this.isLoading = false,
+    this.isLastPage = false,
+    this.page = 1,
   });
 
   TownSection copyWith({
     SectionInfo? info,
     List<Post>? posts,
     List<Category>? childCategories,
+    bool? isLoading,
+    bool? isLastPage,
+    int? page,
   }) {
     return TownSection(
       info: info ?? this.info,
       posts: posts ?? this.posts,
       childCategories: childCategories ?? this.childCategories,
+      isLoading: isLoading ?? this.isLoading,
+      isLastPage: isLastPage ?? this.isLastPage,
+      page: page ?? this.page,
     );
   }
 }
