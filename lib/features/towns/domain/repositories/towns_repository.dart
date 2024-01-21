4efbf749b3c20933515f909acc33b984a705c040
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-20 22:55:52 -0500

    feat(repository): add optional parameters to getNewerPosts
    Updated the `getNewerPosts` method in `TownsRepository` to include optional parameters:
    - `section` for filtering by section.
    - `sectionChilds` for specifying a list of section child IDs.
    
    This enhancement provides more flexibility in querying newer posts based on townCategoryId and additional filtering options.

--- lib/features/towns/domain/repositories/towns_repository.dart
+++ lib/features/towns/domain/repositories/towns_repository.dart
@@ -4,6 +4,7 @@ import 'package:puebly/features/towns/domain/entities/town.dart';
 
 abstract class TownsRepository {
   Future<List<Town>> getTowns(int page);
-  Future<Map<int, List<Post>>> getNewerPosts(int townCategoryId,int page);
+  Future<Map<int, List<Post>>> getNewerPosts(int townCategoryId, int page,
+      {int? section, List<int>? sectionChilds});
   Future<Map<int, List<Category>>> getSectionChildCategories(int townCategoryId);
 }
