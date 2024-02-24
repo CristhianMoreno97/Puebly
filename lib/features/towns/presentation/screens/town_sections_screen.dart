0c0456e1769f39f12624ed46e358e5dd046de1d1
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-24 10:26:07 -0500

    refactor(towns): remove unnecessary imports

--- lib/features/towns/presentation/screens/town_sections_screen.dart
+++ lib/features/towns/presentation/screens/town_sections_screen.dart
@@ -1,4 +1,3 @@
-import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
