6f2dab073bbf72a7af0902d69619164e668a4f55
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-27 09:52:59 -0500

    refactor(towns): Improve PostCard widget structure and category display
    Refactored the PostCard widget in the `lib/features/towns/presentation/widgets/post_card.dart` file to improve its structure and enhance the display of post categories.
    
    Changes include:
    - Extracted the logic for retrieving post category names into a private method `_getPostCategoryNames`, improving code encapsulation and readability.
    - Adjusted the layout of the PostCard widget to display post title and categories in a more organized manner.
    - Introduced the `_PostCategories` widget to handle the display of post categories, allowing for horizontal scrolling when multiple categories are present.
    - Implemented dynamic scrolling to the left in `_PostCategories` when there are multiple categories, enhancing the user experience and ensuring all categories are visible.
    - Refactored the styling of post categories using the `_Badget` widget, ensuring consistent and visually appealing presentation.

--- lib/features/towns/presentation/widgets/post_card.dart
+++ lib/features/towns/presentation/widgets/post_card.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:go_router/go_router.dart';
 import 'package:puebly/config/theme/color_manager.dart';
 import 'package:puebly/features/towns/domain/entities/post.dart';
@@ -9,6 +10,17 @@ import 'package:puebly/features/towns/presentation/providers/post_provider.dart'
 class PostCard extends ConsumerWidget {
   final Post? post;
   const PostCard({super.key, this.post});
+
+  List<String> _getPostCategoryNames(List<Map<String, dynamic>>? categories) {
+    if (categories == null) {
+      return [];
+    }
+
+    return categories
+        .map<String>((category) => category['name'] ?? '')
+        .where((name) => name.isNotEmpty)
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -30,19 +42,102 @@ class PostCard extends ConsumerWidget {
               offset: const Offset(0, 0),
             ),
           ],
-          border: Border.all(
-            color: ColorManager.pueblyPrimary1.withOpacity(0.2),
-            width: 2,
-            strokeAlign: BorderSide.strokeAlignOutside,
-          ),
         ),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _ImageViewer(post?.featuredImgUrl),
-            Expanded(child: _TextContent(post?.title)),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _TextContent(post?.title),
+                    const SizedBox(height: 4),
+                    _PostCategories(
+                      categories: _getPostCategoryNames(post?.categories),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _PostCategories extends StatefulWidget {
+  const _PostCategories({required this.categories});
+
+  final List<String> categories;
+
+  @override
+  State<_PostCategories> createState() => _PostCategoriesState();
+}
+
+class _PostCategoriesState extends State<_PostCategories> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.categories.length > 1) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (_scrollController.hasClients) {
+          //_scrollController.jumpTo(0);
+          _scrollController.animateTo(0,
+              duration: const Duration(seconds: 2), curve: Curves.ease);
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.categories.isEmpty
+        ? const SizedBox()
+        : SizedBox(
+            height: 32,
+            child: MasonryGridView.count(
+              scrollDirection: Axis.horizontal,
+              crossAxisCount: 1,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              controller: _scrollController,
+              itemCount: widget.categories.length,
+              itemBuilder: (context, index) {
+                final category = widget.categories[index];
+                return _Badget(text: category);
+              },
+            ),
+          );
+  }
+}
+
+class _Badget extends StatelessWidget {
+  const _Badget({
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      decoration: const BoxDecoration(
+        color: ColorManager.brightYellowTint1,
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+      ),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              color: ColorManager.brightYellowShade2,
+              fontWeight: FontWeight.w500,
+            ),
       ),
     );
   }
@@ -97,16 +192,6 @@ class _TextContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      child: Text(
-        text ?? '',
-        style: const TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.w400,
-          color: Colors.black,
-        ),
-      ),
-    );
+    return Text(text ?? '', style: Theme.of(context).textTheme.titleSmall);
   }
 }
