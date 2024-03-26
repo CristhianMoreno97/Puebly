0610fe1edd73bcbb341a18b8b4fe546d462eb09e
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-03-26 17:18:48 -0500

    feat(analytics): create custom logEvents

--- lib/features/analytics/services/analytics_service.dart
+++ lib/features/analytics/services/analytics_service.dart
@@ -19,8 +19,48 @@ class AnalyticsService {
     return _instance.logEvent(
       name: 'selected_section',
       parameters: {
-        'page_name': name
-      }
+        'page_name': name,
+      },
+    );
+  }
+
+  static void selectedPost(String name, int id) async {
+    return _instance.logEvent(
+      name: 'selected_post',
+      parameters: {
+        'post_name': name,
+        'post_id': id,
+      },
+    );
+  }
+
+  static void selectedWhatsappButton(String postName, int postId) async {
+    return _instance.logEvent(
+      name: 'selected_whatsapp_button',
+      parameters: {
+        'post_name': postName,
+        'post_id': postId,
+      },
+    );
+  }
+
+  static void selectedCallButton(String postName, int postId) async {
+    return _instance.logEvent(
+      name: 'selected_call_button',
+      parameters: {
+        'post_name': postName,
+        'post_id': postId,
+      },
+    );
+  }
+
+  static void selectedLocationButton(String postName, int postId) async {
+    return _instance.logEvent(
+      name: 'selected_location_button',
+      parameters: {
+        'post_name': postName,
+        'post_id': postId,
+      },
     );
   }
 }
