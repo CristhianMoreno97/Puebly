f15f8d758c4f5f53487dbeb922f0cc486f25fbb7
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-17 18:00:32 -0500

    feat(widget): crear image_viewer_widget

---
+++ lib/features/shared/presentation/widgets/image_viewer_widget.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ImageViewerWidget extends StatelessWidget {
+  final String image;
+
+  const ImageViewerWidget(this.image, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (image == '') {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.asset(
+          'assets/images/puebly-loader.gif',
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+
+    late ImageProvider imageProvider;
+    if (image.startsWith('http') || (image.contains('http') && kIsWeb) ) {
+      imageProvider = NetworkImage(image);
+    } else {
+      imageProvider = FileImage(File(image));
+    }
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: FadeInImage(
+        image: imageProvider,
+        fit: BoxFit.cover,
+        placeholder: const AssetImage('assets/images/placeholder_2.png'),
+      ),
+    );
+  }
+}
